471391cab6f45c3ec8ffc2385efa445990ae7b0f
Kelvin Jin <kelvinjin@google.com>
2018-01-10 17:26:34 -0800

    updated translate demo

--- gae_translate/server.js
+++ gae_translate/server.js
@@ -1,43 +1,19 @@
-/**
- * Import the modules we need to use.
- */
-
-// A web server framework.
+// Import the "express" module.
 const express = require('express');
-// A convenience plugin for express.
-const bodyParser = require('body-parser');
-// The Google Cloud Translate library.
+// Import the Google Cloud Translate library.
 const GoogleCloudTranslate = require('@google-cloud/translate');
 
-/**
- * Initialize Google Cloud Translate.
- */
-
+// Initialize Google Cloud Translate.
 const translate = new GoogleCloudTranslate();
 
-/**
- * Set up the server.
- */
-
-// process.env contains an object with environmental variables.
-// In Google App Engine, the environmental variable PORT will automatically
-// be set.
-const port = process.env.PORT || 8080;
-
-// Create a server
-const app = express();
-app.use(bodyParser());
-app.set('view engine', 'pug');
-
-// Handle an incoming GET request for the root path
-app.get('/', function handleRoot(request, response) {
+function handleRoot(request, response) {
   response.render('index');
-});
+}
 
-// Handle an incoming POST request for translate
-app.post('/translate', function handleTranslate(request, response) {
+function handleTranslate(request, response) {
   // Pre-condition: check that we actually received text.
-  if (!request.body.textToTranslate) {
+  const query = request.query;
+  if (!query.text) {
     response.statusCode = 400; // Bad request
     response.send('Expected text to translate, but didn\'t get any.');
     return;
@@ -45,22 +21,32 @@ app.post('/translate', function handleTranslate(request, response) {
 
   // Translate the text. The translation itself it asynchronous because we need
   // to wait for the server to do the translation.
-  const textToTranslate = request.body.textToTranslate;
+  const textToTranslate = request.query.text;
   const translating = translate.translate(textToTranslate, {
-    from: 'en',
-    to: 'es'
+    from: query.from,
+    to: query.to
   });
 
   // Define what we want to do after the translation is done.
   translating.then(function handleTranslatedText([translatedText]) {
-    console.log(`Translated: "${textToTranslate}" -> "${translatedText}"`);
+    console.log(`Translated: "${textToTranslate}" [${query.from}] -> "${translatedText}" [${query.to}]`);
     response.send(translatedText);
   }).catch(function handleTranslationError(error) {
     response.statusCode = 500; // Server error
     console.error(error.stack);
     response.send(`Server encountered an error: ${error.message}`);
   });
-});
+}
 
-app.listen(port);
-console.log(`Now serving on http://localhost:${port}`);
+// Create a new HTTP web application.
+const app = express();
+// Configure the view engine.
+app.set('view engine', 'pug');
+// When someone hits the root path, respond according to handleRoot.
+app.get('/', handleRoot);
+app.get('/translate', handleTranslate);
+// Determine the TCP port number to listen on from the environment, defaulting to 8080.
+const PORT = process.env.PORT || 8080;
+// Start the web application, listening the port number determined above.
+app.listen(PORT);
+console.log(`Now listening on port ${PORT}... Press Ctrl+C to quit.`);
