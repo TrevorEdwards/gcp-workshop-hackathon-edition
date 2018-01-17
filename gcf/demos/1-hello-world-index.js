ae52add30c64997850f62060075e804c04ad7d9a
Trevor Edwards <trevoredwards@google.com>
2018-01-16 22:07:39 -0800

    update demo

--- gcf/demos/1-hello-world-index.js
+++ gcf/demos/1-hello-world-index.js
@@ -1,15 +1,10 @@
 /**
- * Responds to any HTTP request that can provide a "name" field in the body.
- *
- * @param {!Object} req Cloud Function request context.
- * @param {!Object} res Cloud Function response context.
- */
-exports.helloName = (req, res) => {
-  // Example input: {"name": "Trevor"}
-  if (req.body.name === undefined) {
-    res.status(400).send('Missing name!'); // This is an error case, as "name" is required.
-  } else {
-    res.status(200).send('Hello ' + req.body.name);
-  }
+ *  * Responds to any HTTP request with 'Hello world!'
+ *   *
+ *    * @param {!Object} req Cloud Function request context.
+ *     * @param {!Object} res Cloud Function response context.
+ *      */
+exports.helloWorld = (req, res) => {
+      res.status(200).send('Hello world!'); // Send response with 200 (OK) status.
 };
 
